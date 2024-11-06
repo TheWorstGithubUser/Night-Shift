ee59d709138259b2b7fcdcfc0323e1e56459e5a6
BenHaeck <84250841+BenHaeck@users.noreply.github.com>
2024-11-06 17:19:07 -0500

    Update Interactable.cs

--- Interactable.cs
+++ Interactable.cs
@@ -3,19 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour{
-    public static List<Interactable> interactables = new List<Interactable>();
+    static List<Interactable> interactables = new List<Interactable>();
+
+    
+    [SerializeField] SpriteRenderer popup;
+    Vector3 popupPos;
+    Vector3 popupHidePos = Vector3.zero;
+    Vector3 popupScale;
+    float popupPhase = 0;
+
+    public bool active = true;
+    public Items requiredItems = Items.None;
+
+    Inventory inventory;
+
+
+    bool hasCorrectItems = true;
+
+    const float selectionTime = 0.25f;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
+        inventory = FindAnyObjectByType<Inventory> ();
         interactables.Add (this);
+        if (popup != null) {
+            popupScale = popup.transform.localScale;
+            popupPos = popup.transform.localPosition;
+            
+        }
 		//SendMessage ("OnPlayerInteract", null, SendMessageOptions.DontRequireReceiver);
 	}
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+        if (popup != null) {
+			popupPhase = Mathf.Clamp (popupPhase, 0, 1);
+            popup.transform.localScale = (Mathf.Exp(popupPhase)-1)/Mathf.Exp(1) * popupScale;
+            popup.transform.localPosition = Vector3.Lerp(popupHidePos, popupPos, Mathf.SmoothStep(0, 1, popupPhase));
+			popupPhase -= Time.deltaTime / selectionTime;
+            var col = popup.color;
+            if (inventory != null)
+                hasCorrectItems = inventory.CheckItem (requiredItems);
+            col.a = hasCorrectItems ? 1 : 0.5f;
+            popup.color = col;
+        }
         
     }
+
+    public void WhenSelected () {
+        popupPhase += 2 * Time.deltaTime / selectionTime;
+    }
+
+    public void TriggerInteraction () {
+        if (!active || !hasCorrectItems) return;
+		gameObject.SendMessage ("OnPlayerInteract", null, SendMessageOptions.DontRequireReceiver);
+	}
 
 	private void OnDestroy () {
         interactables.Remove (this);
@@ -26,6 +67,7 @@ public class Interactable : MonoBehaviour{
         Interactable? res = null;
 
         for (int i = 0; i < interactables.Count; i++) {
+            if (!interactables[i].active) continue;
             float dist = Vector2.Distance (searchPos, interactables[i].transform.position);
 
             if (dist < radius && dist < currentDist) {
@@ -36,4 +78,10 @@ public class Interactable : MonoBehaviour{
 
         return res;
     }
+}
+
+public enum InteractableLock {
+    Unlocked,
+    VisibleLock,
+    HideLock,
 }
