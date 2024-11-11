10cc9d1afe39a532aa935826e008215786985e7d
BenHaeck <84250841+BenHaeck@users.noreply.github.com>
2024-11-10 20:58:40 -0500

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,29 +14,32 @@ public class Enemy : MonoBehaviour
 
     public GameObject player;
 
+    public Transform[] waypoints;
+
     private AIPather pather;
     public AIPather Pather => pather;
 
-    public Transform[] waypoints;
+	public float increment = 1/3f;
 
-    public float maxAngle;
+	public float maxAngle;
     public float maxRadius;
 
     public float walk = 2f;
     public float run = 5f;
 
-    private float time;
-    public float increment = 1/3f;
-    public bool RemembersPlayer => time <= 1;
-
     public int currentIndex = 0;
 
     public bool seesPlayer = false;
 
-    Vector2 facingDirection;
+    private Vector2 facingDirection;
+	private float time;
+    public bool RemembersPlayer => time <= 1;
 
     public float pushRay = 0.5f;
     public float pushRayDistance = 1;
+
+    public int pathDepth = 3;
+
     private float waitTime = 0;
     public bool IsWaiting => waitTime > 0;
 
@@ -47,79 +51,68 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         target = waypoints[currentIndex].transform.position;
-        time = 3f;
-        //increment = 1 / 60f;
-        
         pather = FindObjectOfType<AIPather> ();
     }
 
     void Update()
     {
-        seesPlayer = SpotPlayer();
         float movementSpeed = run;
+        seesPlayer = SpotPlayer();
         if (waitTime <= 0) {
-            if (!seesPlayer)
-            {
-                if (!RemembersPlayer)
-                {
+            if (!seesPlayer) {
+                if (!RemembersPlayer) {
                     if (autoTarget)
-                        SetTarget(waypoints[currentIndex].transform.position);
-                    //transform.position = Vector2.MoveTowards(transform.position, target, walk * Time.deltaTime);
+                        SetTarget (waypoints[currentIndex].transform.position);
+                    //transform.position = Vector2.MoveTowards (transform.position, target, walk * Time.deltaTime);
                     movementSpeed = walk;
-    
-                    if (Vector2.Distance(transform.position, waypoints[currentIndex].transform.position) < 1)
-                    {
+
+                    if (Vector2.Distance (transform.position, waypoints[currentIndex].position) < 1) {
                         currentIndex = (currentIndex + 1) % waypoints.Length;
                         target = waypoints[currentIndex].transform.position;
                     }
                 }
-                else
-                {
+                else {
                     if (autoTarget)
-                        SetTarget(player.transform.position);
-                    //transform.position = Vector2.MoveTowards(transform.position, target, run * Time.deltaTime);
+                        SetTarget (player.transform.position);
+                    //transform.position = Vector2.MoveTowards (transform.position, target, run * Time.deltaTime);
                 }
                 time += Time.deltaTime * increment;
                 //Debug.Log(time);
             }
-    
+
+
             //else chase player
-            else
-            {
+            else {
+                //autoTarget = true;
                 if (autoTarget)
-                    SetTarget(player.transform.position);
-                transform.position = Vector2.MoveTowards(transform.position, target, run * Time.deltaTime);
+                    SetTarget (player.transform.position);
+                //transform.position = Vector2.MoveTowards (transform.position, target, run * Time.deltaTime);
                 time = 0;
             }
-        }
-
-        if (pather != null) {
-            Vector2 dir = pather.FindPath (transform.position, target, 4);
-            Vector2 sideDir = new Vector2 (-dir.y, dir.x);
-            Vector2 walkDir = dir;
-            for (int d = -1; d <= 1; d+=2) {
-                if (Physics2D.Raycast (transform.position, dir + sideDir * pushRay * d, pushRayDistance, pather.GetObstacleMask ()).collider != null) {
-                    walkDir -= sideDir * pushRay * d;
+            if (pather != null) {
+                Vector2 dir = pather.FindPath (transform.position, target, pathDepth);
+                Vector2 sideDir = new Vector2 (-dir.y, dir.x);
+                Vector2 walkDir = dir;
+                for (int d = -1; d <= 1; d+=2) {
+                    if (Physics2D.Raycast (transform.position, dir + sideDir * pushRay * d, pushRayDistance, pather.GetObstacleMask ()).collider != null) {
+                        walkDir -= sideDir * pushRay * d;
+                    }
                 }
+                walkDir.Normalize ();
+                currentMovementVelocity = walkDir * movementSpeed;
+                transform.position += (Vector3)CurrentMovementVelocity * Time.deltaTime;
             }
-            walkDir.Normalize ();
-            currentMovementVelocity = walkDir * movementSpeed;
-            transform.position += (Vector3)CurrentMovementVelocity * Time.deltaTime;
+            else {
+                transform.position = Vector2.MoveTowards (transform.position, target, movementSpeed * Time.deltaTime);
+
+            }
         }
-        else {
-            transform.position = Vector2.MoveTowards (transform.position, target, movementSpeed * Time.deltaTime);
+
         
-        }
 
-        facingDirection = (target - transform.position).normalized;
-    }
-
-    public void ForceTarget (Vector3 t) {
-        SetTarget (t);
-        autoTarget = false;
-    }
-    
-    public void EnableAutoTarget () { autoTarget = true; }
+		facingDirection = (target - transform.position).normalized;
+        waitTime = MathF.Max(waitTime - Time.deltaTime, 0);
+	}
 
     void SetTarget(Vector3 t)
     {
@@ -130,28 +123,33 @@ public class Enemy : MonoBehaviour
         return waypoints[currentIndex].position;
     }
 
+    public void ForceTarget (Vector3 t) {
+        SetTarget (t);
+        autoTarget = false;
+    }
+
     public void MakeWait (float time) {
         waitTime = time;
     }
 
+    public void EnableAutoTarget () { autoTarget = true; }
+
     bool SpotPlayer()
     {
-        
+        //test if it can switch by using distance
         bool facingPlayer = false;
         Vector2 playerDirection = (player.transform.position - transform.position).normalized;
-        if (seesPlayer)
-        {
+        if (seesPlayer) {
             facingPlayer = true;
         }
-        else
-        {
-            facingPlayer = Vector2.Dot(playerDirection, facingDirection) > Mathf.Cos(maxAngle * Mathf.Deg2Rad);
+        else {
+            facingPlayer = Vector2.Dot (playerDirection, facingDirection) > Mathf.Cos(maxAngle * Mathf.Deg2Rad);
         }
 
-        var res = Physics2D.Raycast(transform.position, playerDirection, Vector2.Distance(player.transform.position, transform.position), obstacleMask);
+        var res = Physics2D.Raycast (transform.position, playerDirection, Vector2.Distance(player.transform.position, transform.position), obstacleMask);
 
-        return Vector3.Distance(transform.position, player.transform.position) < maxRadius && facingPlayer && res.collider == null;
+        return Vector3.Distance (transform.position, player.transform.position) < maxRadius && facingPlayer && res.collider == null;
+        
     }
 
-    
 }
