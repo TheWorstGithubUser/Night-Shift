85b4640386f748976723c8c156a196d615cc8438
calebabney7 <calebabney45@gmail.com>
2024-11-11 22:31:13 -0500

    Update FieldOfView.cs

--- FieldOfView.cs
+++ FieldOfView.cs
@@ -8,10 +8,11 @@ public class FieldOfView : MonoBehaviour
 {
     [SerializeField] private LayerMask layerMask;
     [SerializeField] private GameObject Player;
-    private float fov = 40f;
+    public float fov;
     private Mesh mesh;
     Vector3 origin;
     private float startingAngle;
+    private PolygonCollider2D polygonCollider;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,9 @@ public class FieldOfView : MonoBehaviour
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         origin = Player.transform.position;
+        polygonCollider = GetComponent<PolygonCollider2D>();
     }
+
     private void LateUpdate()
     {
         //set values
@@ -27,21 +30,26 @@ public class FieldOfView : MonoBehaviour
         int rayCount = 50;
         float angle = startingAngle;
         float angleIncrease = fov / rayCount;
-        float viewDistance = 10f;
+        float viewDistance = 7f;
 
 
         Vector3[] vertices = new Vector3[rayCount + 1 + 1];
         Vector2[] uv = new Vector2[vertices.Length];
+        Vector2[] colliderPoints = new Vector2[vertices.Length];
         int[] triangles = new int[rayCount * 3];
 
         vertices[0] = origin;
+        colliderPoints[0] = new Vector2(origin.x, origin.y);
 
         int vertexIndex = 1;
         int triangleIndex = 0;
 
         for (int i = 0; i <= rayCount; i++)
         {
+            //array for mesh
             Vector3 vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+            //array for collider
+            Vector2 colliderVertex =  new Vector2(vertex.x, vertex.y);
             RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance, layerMask);
 
             if (raycastHit2D.collider == null)
@@ -52,9 +60,11 @@ public class FieldOfView : MonoBehaviour
             {
                 //hit
                 vertex = raycastHit2D.point;
+                colliderVertex = new Vector2(vertex.x, vertex.y);
             }
 
             vertices[vertexIndex] = vertex;
+            colliderPoints[vertexIndex] = colliderVertex;
 
             if (i > 0)
             {
@@ -73,6 +83,7 @@ public class FieldOfView : MonoBehaviour
         mesh.uv = uv;
         mesh.triangles = triangles;
         mesh.bounds = new Bounds(origin, Vector3.one * 1000f);
+        polygonCollider.SetPath(0, colliderPoints);
 
 
     }
@@ -98,5 +109,5 @@ public class FieldOfView : MonoBehaviour
         float angleRad = angle * (Mathf.PI / 180f);
         return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
     }
-
 }
+
