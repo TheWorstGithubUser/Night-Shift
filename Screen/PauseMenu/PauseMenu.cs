da8eb29f1daa4994eaf275e241f7a7bceb871da9
GamerChan756 <116231818+GamerChan756@users.noreply.github.com>
2024-11-14 12:48:27 -0500

    Add files via upload

--- Screen/PauseMenu/PauseMenu.cs
+++ Screen/PauseMenu/PauseMenu.cs
@@ -43,6 +43,7 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Debug.Log("Loading menu...");
+        GameIsPaused = false;
         SceneManager.LoadScene(0);
 
     }
