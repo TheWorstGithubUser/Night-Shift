ceeca954f23a91a2a4201bd5b5036a4b3debdc43
calebabney7 <calebabney45@gmail.com>
2024-11-16 14:30:12 -0500

    Update TaskScript.cs

--- TaskScript.cs
+++ TaskScript.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class TaskScript : MonoBehaviour{
-    bool complete = false;
+public class TaskScript : MonoBehaviour
+{
+    public bool complete = false;
     public bool Complete => complete;
     [SerializeField] Sprite completedSprite;
 
@@ -13,21 +16,23 @@ public class TaskScript : MonoBehaviour{
 
     public string displayName = "";
     // Start is called before the first frame update
-    void Start(){
-        inventory = FindObjectOfType<Inventory> ();
-        interactable = GetComponent <Interactable>();
+    void Start()
+    {
+        inventory = FindObjectOfType<Inventory>();
+        interactable = GetComponent<Interactable>();
     }
 
     // Update is called once per frame
-    void Update(){
-        
+    void Update()
+    {
+
     }
 
-    void OnPlayerInteract () {
-        //if (inventory != null) if (!inventory.CheckItem (requiredItem)) return;
-
-        complete = true;
-        if (completedSprite != null && renderer != null) {
+    //call this once task is complete
+    public void IsComplete()
+    {
+        if (completedSprite != null && renderer != null)
+        {
             interactable.active = false;
             renderer.sprite = completedSprite;
         }
